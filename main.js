c5e479e88fb30a1c944306d96050ac89886191a2
Liz Dominguez <liz.dominguez.leon@gmail.com>
2017-02-21 18:56:27 -0500

    fixed some JS Lint

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ var icons = [
   'url("icons/png/moon-1.png")'
 ];
 
-var Coordinates = function(lat, lon) {
+var Coordinates = function (lat, lon) {
   this.latitude = lat;
   this.longitude = lon;
 };
@@ -25,18 +25,17 @@ function getLocation() {
   
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
-  } 
-  
-  else {
-    display.innerHTML = 'Geolocation is not supported by this browser.';
+  }   
+    else {
+      display.innerHTML = 'Geolocation is not supported by this browser.';
   }
-};
+}  
 
 function showPosition(position) {
   coord.latitude = position.coords.latitude;
   coord.longitude = position.coords.longitude;
   callWeatherAPI();
-};
+}
 
 
 function getURL(lat, lon, units) {
@@ -46,18 +45,18 @@ function getURL(lat, lon, units) {
   return 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat +
     '&lon=' +
     lon + '&units=' + units + '&appid=' + appid;
-};
+}
 
 function callWeatherAPI() {
   var url = getURL(coord.latitude, coord.longitude, units);
   $.getJSON(url, getWeather);
-};
+}
 
 function getWeather(data) {
   var city = data.name;
   var temp = data.main.temp;
   var tempUnit = units === 'metric' ? 'C' : 'F';
-  var humidity = data.main.humidity; 
+  var humidity = data.main.humidity;
   var wind = data.wind.speed;
   var windUnit = units === 'metric' ? ' meters/sec' : ' mph';
   var conditions = data.weather[0].main;
@@ -80,7 +79,7 @@ function getWeather(data) {
   dayOrNight(dt, sunset, sunrise);
 
 
-};
+}
 
 function setBackground(temp) {
   if (temp >= 90) {//hot
@@ -101,24 +100,21 @@ function setBackground(temp) {
     $('.night-day').css('left', '180px');
     $('.forecast').css('text-shadow', '0px 2px 2px #6f56cf');
   }
-};
+}
 
 function dayOrNight(dt, sunset, sunrise) {
-  if( dt >= sunrise && dt < sunset) { //day
+  if (dt >= sunrise && dt < sunset) { //day
     $('.night-day').css('background-image', icons[4]);
   }
   
-  else {//night
-  $('.night-day').css('background-image', icons[5]);
-}
+    else {//night
+      $('.night-day').css('background-image', icons[5]);
+    }
     
 }
 
 
-$(document).ready(function() {
-  
-    getLocation();
-    callWeatherAPI();
-  
-  
+$(document).ready(function () {
+  getLocation();
+  callWeatherAPI();
 });
